908e12606ba803f9aa76a0411e50ec6f7f695207
natan a fonseca <khanpessoal@gmail.com>
2025-01-15 21:48:26 -0300

    xinqui

--- ex0007/Program.cs
+++ ex0007/Program.cs
@@ -6,11 +6,25 @@ namespace ex0007
     {
         static void Main(string[] args)
         {
-            double nota_final;
+            double nota_final, n1, n2, n3, n4;
             string resultado;
 
-            Console.Write(" digite a nota final do aluno");
-            nota_final = Convert.ToDouble(Console.ReadLine());
+            Console.Write(" digite a primeira nota do aluno ");
+            n1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write(" digite a segunda nota do aluno ");
+            n2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write(" digite a terceira nota do aluno ");
+            n3 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write(" digite a quarta nota do aluno ");
+            n4  = Convert.ToDouble(Console.ReadLine());
+
+            nota_final = (n1 + n2 + n3 + n4) / 4;
+
+
+
 
             if (nota_final >= 70)
             {
@@ -41,7 +55,7 @@ namespace ex0007
             
             Console.WriteLine("nota do aluno: {0} -  resultado: {1}",nota_final, resultado);
 
-           
+            
         }
 
 
