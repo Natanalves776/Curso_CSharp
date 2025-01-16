dda5a9638026940c8620d8d6b41588db33c4f01d
natan a fonseca <khanpessoal@gmail.com>
2025-01-15 22:17:07 -0300

    radar

---
+++ ex 009/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ex_009
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int velocidade;
+
+           Console.WriteLine(" me diga sua velocidade");
+            velocidade = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+        }
+    }
+}
