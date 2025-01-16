71464be1af050aa4bfe78327ca7f5f268600c57f
natan a fonseca <khanpessoal@gmail.com>
2025-01-15 22:25:13 -0300

    numero par

---
+++ ex0010/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ex0010
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n3, n4, n5;
+            float numero;
+
+            if (numero) =>
+               
+
+
+
+
+
+            Console.WriteLine(" digite um numero");
+            nota = Convert.ToInt32 (Console.ReadLine());
+        }
+    }
+}
