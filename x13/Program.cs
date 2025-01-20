06be0af61cc786f8b6c1ec30315921815239fca3
natan a fonseca <khanpessoal@gmail.com>
2025-01-20 20:04:58 -0300

    conclusao do projeto 20 /01

--- x13/Program.cs
+++ x13/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace x13
+﻿using System.ComponentModel.Design;
+
+namespace x13
 {
     internal class Program
     {
@@ -15,7 +17,20 @@
             Console.Write(" digite o valor do lado c:  ");
             ladoC = Convert.ToInt32(Console.ReadLine());
 
+            if ((ladoA < ladoB + ladoC) && (ladoB < ladoA + ladoC) && (ladoC < ladoB + ladoA))
 
+
+            {
+                Console.WriteLine(" os valores informados forman um triangulo ");
+            }
+
+
+
+
+            else
+            {
+                Console.WriteLine("os valores informados não foram um trinangulo");
+            }
 
         }
     }
