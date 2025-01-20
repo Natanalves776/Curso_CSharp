9f4d855ca1e45646ff9e59f5ba9f51a2c4517f30
natan a fonseca <khanpessoal@gmail.com>
2025-01-20 19:00:21 -0300

    par impar

---
+++ ex12/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ex12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+
+            Console.WriteLine("Qual o numero? ");
+
+            num = Convert.ToInt32(Console.ReadLine());
+
+            if (num % 2 == 0)
+
+            {
+
+                Console.WriteLine("Par {0}", num);
+
+            }
+
+            else
+
+            {
+
+                Console.WriteLine("impar {0}", num);
+
+            }
+
+        }
+    }
+}
