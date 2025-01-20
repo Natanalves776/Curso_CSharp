fbcd46a5fc04bf18b4b5ff9f3f9bc4d75f893d79
natan a fonseca <khanpessoal@gmail.com>
2025-01-20 19:30:41 -0300

    x13 projeto aula 20/01

---
+++ x13/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace x13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int ladoA, ladoB, ladoC;
+
+            Console.Write(" digite o valor do lado A:  ");
+            ladoA = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write(" digite o valor do lado B:  ");
+            ladoB = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write(" digite o valor do lado c:  ");
+            ladoC = Convert.ToInt32(Console.ReadLine());
+
+
+
+        }
+    }
+}
