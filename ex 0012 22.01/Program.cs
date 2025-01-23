090aa576184dcf8ed176a551a6d4dd90d6d1ba94
natan a fonseca <khanpessoal@gmail.com>
2025-01-22 21:02:22 -0300

    novos ex de 22/01

---
+++ ex 0012 22.01/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ex_0012_22._01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int final;
+
+            Console.WriteLine("´´´´´´´´´´´´´´´´´´´´´´´´´´´´");
+            Console.WriteLine("digite o final do intervalo");
+            Console.WriteLine("´´´´´´´´´´´´´´´´´´´´´´´´´´´´");
+            Console.WriteLine();
+
+            Console.WriteLine("digite o final do intervalo");
+            final = Convert.ToInt32(Console.ReadLine());
+            
+                for (int i = 1; i <= final; i++)
+             
+                if (i % 2 == 0)
+            {
+                    Console.Write(" {0}, ", i);
+            }
+        }
+
+    }
+}
---
+++ ex 15/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ex_15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 10; i > 0; i--)
+            {
+                Console.WriteLine("¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨");
+                Console.WriteLine("contagem regressiva ");
+                Console.WriteLine("¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨");
+                {
+                    Console.WriteLine(i);
+                
+                }
+            }
+            
+        }
+    }
+}
