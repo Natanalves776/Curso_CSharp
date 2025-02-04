7de44361a3fb512fa1804c2fe5fc9ed77cc6f812
natan a fonseca <khanpessoal@gmail.com>
2025-02-03 21:18:04 -0300

    exec dia 03/02/25

---
+++ fatorial/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace fatorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            char resp;
+            bool mostrar = false;
+
+            Console.Write("digite um numero inteiro: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            Console.Write("deseja mostra os calculos? (s/n)");
+            resp = Convert.ToChar(Console.ReadLine().ToLower()); // sempre que usar TOlower sempre deixara as letras minusculas..
+            Console.WriteLine();
+
+            if (resp == 's') { mostrar = true; }
+
+            Console.WriteLine(fatorial(num, mostrar));
+
+
+
+
+        }
+
+        static int fatorial(int n, bool show= false)
+        {
+            int f = 1;
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (show)
+                {
+                    Console.Write(i);
+                    if (i < n)
+                    {
+                        Console.Write(" x ");
+                    }
+                    else
+                    {
+                        Console.Write(" = ");
+                    }
+                }
+                f *= i; // esse F representa o fatorial repetitivo até o numero 1 
+            }
+            return f; // sempre que usar int no static deve se por esse codigo return no final.
+        }
+    }
+}
---
+++ votação/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace votação
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int nasc; 
+            Console.WriteLine("digite seu ano de nascimento?   ");
+            nasc = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(voto(nasc));
+
+        }
+
+        static string voto( int ano)
+        {
+            DateTime dataAtual = DateTime.Now;
+           // int dia = dataAtual.Day;
+           // int mes = dataAtual.Month;
+            int  anoAtual = dataAtual.Year;
+            int  idade = anoAtual - ano;
+
+            if (idade < 16)
+            {
+                return $"com {idade} anos, vocé não pode votar";
+            }
+            else if (idade > 18 && idade < 70)
+            {
+              return $" {idade} anos, você vota";
+            }
+            else
+            {
+                return $"com {idade} anos, seu voto é opcional";
+            }
+            // int hora = dataAtual.Hour;
+            // int minuto = dataAtual.Minute;
+            // int segundo = dataAtual.Second;
+        }
+    }
+}
